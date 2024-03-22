9efd93465950fe7247157c5c15489cc236acbd9f
karolinapi27 <karo_pi@hotmail.fr>
2024-03-22 17:31:57 +0100

    Ajout des attributs AriaLabelsau footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
       <h3 className={styles.footerTitle}>Contact</h3>
       <div className={styles.contactInfo}>
         <p>Téléphone : 0767860082</p>
-        <p>Email : <a href="mailto:karolinapi@icloud.com">karolinapi@icloud.com</a></p>
-        <p>GitHub : <a href="https://github.com/dashboard" target="_blank" rel="noopener noreferrer"></a></p>
-        <p>LinkedIn : <a href="https://linkedin.com/in/yourProfile" target="_blank" rel="noopener noreferrer"></a></p>
+        <p>Email : <a href="mailto:karolinapi@icloud.com" aria-label="Envoyer un email à karolinapi@icloud.com">karolinapi@icloud.com</a></p>
+        <p>GitHub : <a href="https://github.com/tonUtilisateurGitHub" target="_blank" rel="noopener noreferrer" aria-label="Visitez mon profil GitHub">Visitez mon GitHub</a></p>
+        <p>LinkedIn : <a href="https://linkedin.com/in/tonProfileLinkedIn" target="_blank" rel="noopener noreferrer" aria-label="Visitez mon profil LinkedIn">Visitez mon LinkedIn</a></p>
       </div>
       <p>© 2024 Tous droits réservés</p>
     </footer>
