dd94ad5df17af2b0186a6d7a4df1c238f9dc2f23
karolinapi27 <karo_pi@hotmail.fr>
2024-03-23 16:45:15 +0100

    Ajustement du header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import styles from './header.module.scss';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleNavLinkClick = (event, anchorId) => {
+    event.preventDefault();
+    const path = `/`;
+
+    if (window.location.pathname === path) {
+      // Si nous sommes déjà sur la page d'accueil, fait défiler vers l'ancre
+      const anchorElement = document.getElementById(anchorId);
+      if (anchorElement) {
+        anchorElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    } else {
+      // Si nous ne sommes pas sur la page d'accueil, navigue puis fait défiler
+      navigate(path, { replace: true });
+      setTimeout(() => {
+        const anchorElement = document.getElementById(anchorId);
+        if (anchorElement) {
+          anchorElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }, 0); 
+    }
+  };
+
   return (
     <header className={styles.header}>
       <NavLink to="/">
@@ -10,10 +34,10 @@ const Header = () => {
       </NavLink>
       <nav>
         <ul className={styles.navbar}>
-          <li><a href="#about" className={styles.navLink}>About me</a></li>
-          <li><a href="#projets" className={styles.navLink}>Mes projets</a></li>
-          <li><a href="#competences" className={styles.navLink}>Compétences</a></li>
-          <li><a href="#contact" className={styles.navLink}>Contact</a></li>
+          <li onClick={(e) => handleNavLinkClick(e, 'about')} className={styles.navLink}>About me</li>
+          <li onClick={(e) => handleNavLinkClick(e, 'projets')} className={styles.navLink}>Mes projets</li>
+          <li onClick={(e) => handleNavLinkClick(e, 'competences')} className={styles.navLink}>Compétences</li>
+          <li onClick={(e) => handleNavLinkClick(e, 'contact')} className={styles.navLink}>Contact</li>
         </ul>
       </nav>
     </header>
@@ -21,4 +45,3 @@ const Header = () => {
 };
 
 export default Header;
-
