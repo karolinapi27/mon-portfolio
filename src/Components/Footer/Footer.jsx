9d252f8619299b612a2c0a446ee72aa6ee6c6fbe
karolinapi27 <karo_pi@hotmail.fr>
2024-03-24 12:00:29 +0100

    Lien Linkedin

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <p>Téléphone : 0767860082</p>
         <p>Email : <a href="mailto:karolinapi@icloud.com" aria-label="Envoyer un email à karolinapi@icloud.com">karolinapi@icloud.com</a></p>
         <p>GitHub : <a href="https://github.com/karolinapi27" target="_blank" rel="noopener noreferrer" aria-label="Visitez mon profil GitHub">Visitez mon GitHub</a></p>
-        <p>LinkedIn : <a href="www.linkedin.com/in/karolina-p-32b292254" target="_blank" rel="noopener noreferrer" aria-label="Visitez mon profil LinkedIn">Visitez mon LinkedIn</a></p>
+        <p>LinkedIn : <a href="https://www.linkedin.com/in/karolina-p-32b292254" target="_blank" rel="noopener noreferrer" aria-label="Visitez mon profil LinkedIn">Visitez mon LinkedIn</a></p>
       </div>
       <p>© 2024 Tous droits réservés</p>
     </footer>
